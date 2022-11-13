10f25faaec2d743bb0e7b51ebca1e468a4e49054
kayode <officialkayodeolowo@gmail.com>
2022-11-13 08:12:06 -0800

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import {Header} from "./components";
-
+import {Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <div className="w-screen h-auto flex flex-col">
+    <div className="w-screen h-auto flex flex-col bg-primary">
         <Header/>
+       <main>
+          <Routes> 
+
+          </Routes>
+       </main>
      
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,28 +4,34 @@ import {HiOutlineShoppingCart} from 'react-icons/hi'
 
 const Header = () => {
   return (
-    <header className='fixed z-50 w-screen bg-red-400 p-6 px-16'>
+    <header className='fixed z-50 w-screen  p-6 px-16'>
           
           
-           <div className='hidden h-full  md:flex w-full'> 
+           <div className='hidden h-full  md:flex w-full justify-between'> 
                 <div className='flex items-center gap-2'>
                         <img className='object-cover w-8' src='./img/logo.png' alt='logo'/> 
                         <p className='text-headingColor text-xl font-bold'> City </p>
 
                 </div>
-                <ul className='flex items-center gap-8 ml-auto'> 
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Home</li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Menu</li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Home </li>
-                        <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'>Service </li>
-                </ul>
-                <div className='relative flex items-center justify-center'>
-                    <HiOutlineShoppingCart className='text-textColor text-2xl ml-10 cursor-pointer'/>
-                    <div className=' absolute  -top-2 -right-3 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
-                        <p className=' text-xs  text-white font-semibold'>
-                            5 </p>    
-                     </div> 
-                </div> 
+                <div className='flex items-center gap-8'> 
+                    <ul className='flex items-center gap-8  '> 
+                            <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Home</li>
+                            <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Menu</li>
+                            <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'> Home </li>
+                            <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 ease-in-out'>Service </li>
+                    </ul>
+                    <div className='relative flex items-center justify-center'>
+                        <HiOutlineShoppingCart className='text-textColor text-2xl ml-10 cursor-pointer'/>
+                        <div className=' absolute  -top-2 -right-3 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                            <p className=' text-xs  text-white font-semibold'>
+                                5 </p>    
+                        </div> 
+                    </div> 
+
+                    
+                        <img className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' src='./img/avatar.png' alt='user'/>
+                    
+                </div>
             </div>
 
 
